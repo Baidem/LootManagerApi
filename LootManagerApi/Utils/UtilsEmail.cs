249130e8319e4f35adc1d7a0828d1eeed77d1aba
POUPAERT Vincent <baidem@gmail.com>
2023-03-16 15:34:45 +0100

    IsValidateEmailAddressAttribute() + TESTS

---
+++ LootManagerApi/Utils/UtilsEmail.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LootManagerApi.Utils
+{
+    public class UtilsEmail
+    {
+        public static bool IsValidateEmailAddressAttribute(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            if (!new EmailAddressAttribute().IsValid(email))
+                return false;
+
+            int atIndex = email.IndexOf("@");
+            if (atIndex < 0 || atIndex == email.Length - 1)
+                return false;
+
+            string domain = email.Substring(atIndex + 1);
+            if (domain.StartsWith(".") || domain.EndsWith("."))
+                return false;
+
+            if (domain.IndexOf(".") < 0)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ LootManagerApiTests/Utils/UtilsEmailTests.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LootManagerApi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LootManagerApi.Utils.Tests
+{
+    [TestClass()]
+    public class UtilsEmailTests
+    {
+        /// <summary>
+        /// Test empty string.
+        /// </summary>
+        [TestMethod()]
+        public void IsValidateEmailAddressAttributeTest01()
+        {
+            Assert.IsFalse(UtilsEmail.IsValidateEmailAddressAttribute(""));
+        }
+
+        /// <summary>
+        /// Test email is white space
+        /// </summary>
+        [TestMethod()]
+        public void IsValidateEmailAddressAttributeTest02()
+        {
+            Assert.IsFalse(UtilsEmail.IsValidateEmailAddressAttribute("   "));
+        }
+
+        /// <summary>
+        /// Test email = "oneword"
+        /// </summary>
+        [TestMethod()]
+        public void IsValidateEmailAddressAttributeTest03()
+        {
+            Assert.IsFalse(UtilsEmail.IsValidateEmailAddressAttribute("oneword"));
+        }
+
+        /// <summary>
+        /// Test email = "@"
+        /// </summary>
+        [TestMethod()]
+        public void IsValidateEmailAddressAttributeTest04()
+        {
+            Assert.IsFalse(UtilsEmail.IsValidateEmailAddressAttribute("@"));
+        }
+
+        /// <summary>
+        /// Test email = "name@"
+        /// </summary>
+        [TestMethod()]
+        public void IsValidateEmailAddressAttributeTest05()
+        {
+            Assert.IsFalse(UtilsEmail.IsValidateEmailAddressAttribute("name@"));
+        }
+
+        /// <summary>
+        /// Test email = "@domaine"
+        /// </summary>
+        [TestMethod()]
+        public void IsValidateEmailAddressAttributeTest06()
+        {
+            Assert.IsFalse(UtilsEmail.IsValidateEmailAddressAttribute("@domaine"));
+        }
+
+        /// <summary>
+        /// Test email = "name@domaine"
+        /// </summary>
+        [TestMethod()]
+        public void IsValidateEmailAddressAttributeTest07()
+        {
+            Assert.IsFalse(UtilsEmail.IsValidateEmailAddressAttribute("name@domaine."));
+        }
+
+        /// <summary>
+        /// Test email = "@domaine.com"
+        /// </summary>
+        [TestMethod()]
+        public void IsValidateEmailAddressAttributeTest08()
+        {
+            Assert.IsFalse(UtilsEmail.IsValidateEmailAddressAttribute("@domaine.com"));
+        }
+
+        /// <summary>
+        /// Test email = "@.com"
+        /// </summary>
+        [TestMethod()]
+        public void IsValidateEmailAddressAttributeTest09()
+        {
+            Assert.IsFalse(UtilsEmail.IsValidateEmailAddressAttribute("@.com"));
+        }
+
+        /// <summary>
+        /// Test email = "name@domaine.com"
+        /// </summary>
+        [TestMethod()]
+        public void IsValidateEmailAddressAttributeTest10()
+        {
+            Assert.IsTrue(UtilsEmail.IsValidateEmailAddressAttribute("name@domaine.com"));
+        }
+    }
+}
