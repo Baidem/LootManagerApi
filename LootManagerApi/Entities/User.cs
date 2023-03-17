46828384179f1e9640b8b89683bcec4901033494
POUPAERT Vincent <baidem@gmail.com>
2023-03-17 10:46:54 +0100

    User Update + migration

--- LootManagerApi/Entities/User.cs
+++ LootManagerApi/Entities/User.cs
@@ -6,5 +6,7 @@
         public string FullName { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
     }
 }
