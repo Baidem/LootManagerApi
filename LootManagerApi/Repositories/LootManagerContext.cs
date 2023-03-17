126c93781c6bacde0aa33b55c7f6d5e9ad10ca48
POUPAERT Vincent <baidem@gmail.com>
2023-03-17 19:15:30 +0100

    Update default Data Base

--- LootManagerApi/Repositories/LootManagerContext.cs
+++ LootManagerApi/Repositories/LootManagerContext.cs
@@ -15,12 +15,13 @@ namespace LootManagerApi.Repositories
 
             // Default data
             // Users
-            var u1 = new User { Id = 1, FullName = "Jerry Seinfeld", Email = "jerry@aol.com", PasswordHash = BCrypt.Net.BCrypt.HashPassword("userNumber1!") };
-            var u2 = new User { Id = 2, FullName = "George Costanza", Email = "George.Costanza@aol.com", PasswordHash = BCrypt.Net.BCrypt.HashPassword("userNumber1!") };
-            var u3 = new User { Id = 3, FullName = "Elaine Benes", Email = "Elaine.Benes@aol.com", PasswordHash = BCrypt.Net.BCrypt.HashPassword("userNumber1!") };
-            var u4 = new User { Id = 4, FullName = "Cosmo Kramer", Email = "Cosmo.Kramer@aol.com", PasswordHash = BCrypt.Net.BCrypt.HashPassword("userNumber1!") };
+            var u1 = new User { Id = 1, FullName = "test", Email = "test", PasswordHash = BCrypt.Net.BCrypt.HashPassword("test") };
+            var u2 = new User { Id = 2, FullName = "Jerry Seinfeld", Email = "jerry@aol.com", PasswordHash = BCrypt.Net.BCrypt.HashPassword("userNumber1!") };
+            var u3 = new User { Id = 3, FullName = "George Costanza", Email = "George.Costanza@aol.com", PasswordHash = BCrypt.Net.BCrypt.HashPassword("userNumber1!") };
+            var u4 = new User { Id = 4, FullName = "Elaine Benes", Email = "Elaine.Benes@aol.com", PasswordHash = BCrypt.Net.BCrypt.HashPassword("userNumber1!") };
+            var u5 = new User { Id = 5, FullName = "Cosmo Kramer", Email = "Cosmo.Kramer@aol.com", PasswordHash = BCrypt.Net.BCrypt.HashPassword("userNumber1!") };
 
-            modelBuilder.Entity<User>().HasData(new List<User> { u1, u2, u3, u4 });
+            modelBuilder.Entity<User>().HasData(new List<User> { u1, u2, u3, u4, u5 });
            
             base.OnModelCreating(modelBuilder);
         }
