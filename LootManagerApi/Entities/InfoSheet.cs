df8c8ada22ddc8c6948dcf74e4a672fd439254db
POUPAERT Vincent <baidem@gmail.com>
2023-04-07 16:52:22 +0200

    Init InfoSheet

---
+++ LootManagerApi/Entities/InfoSheet.cs
@@ -0,0 +1,14 @@
+﻿namespace LootManagerApi.Entities
+{
+    public class InfoSheet
+    {
+        public int Id { get; set; }
+        public string? Designation { get; set; }
+        public string? Reference { get; set; }
+        public string? BarCode { get; set; }
+        public string? WikiArticle { get; set; }
+
+        // Navigation properties
+
+    }
+}
