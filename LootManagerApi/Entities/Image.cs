c8ebd8a0512cc0b49f87ea16edb3493a6a42391f
POUPAERT Vincent <baidem@gmail.com>
2023-04-07 18:29:14 +0200

    Init Image

---
+++ LootManagerApi/Entities/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace LootManagerApi.Entities
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public string? Caption { get; set; }
+    }
+}
