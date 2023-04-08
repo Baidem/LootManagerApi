3eef915b01ab9c89b500b721ba2fe430d48597d3
POUPAERT Vincent <baidem@gmail.com>
2023-04-08 17:28:13 +0200

    Init InfoSheet Files

---
+++ LootManagerApi/Controllers/InfoSheetController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LootManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoSheetController : ControllerBase
+    {
+    }
+}
