be49463e0e2f81c3af024bbd563c22b8815476d1
POUPAERT Vincent <baidem@gmail.com>
2023-04-08 17:53:56 +0200

    InfoSheetController Regions

--- LootManagerApi/Controllers/InfoSheetController.cs
+++ LootManagerApi/Controllers/InfoSheetController.cs
@@ -3,9 +3,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LootManagerApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]/[Action]")]
     [ApiController]
     public class InfoSheetController : ControllerBase
     {
+        #region DECLARATIONS
+
+        #endregion
+
+        #region CONSTRUCTOR
+
+        #endregion
+
+        #region CREATE
+
+        #endregion
+
+        #region READ
+
+        #endregion
+
+        #region UPDATE
+
+        #endregion
+
+        #region DELETE
+
+        #endregion
+
+        #region LOG
+
+        #endregion
+
+
     }
 }
