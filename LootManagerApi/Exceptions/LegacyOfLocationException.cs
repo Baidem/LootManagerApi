a10ca177cd9378f6e510ad61a19583de0ea10543
POUPAERT Vincent <baidem@gmail.com>
2023-04-24 19:01:44 +0200

    Update : LocationRepository

---
+++ LootManagerApi/Exceptions/LegacyOfLocationException.cs
@@ -0,0 +1,13 @@
+﻿namespace LootManagerApi.Exceptions
+{
+    public class LegacyOfLocationException : Exception
+    {
+        public LegacyOfLocationException()
+        {
+        }
+
+        public LegacyOfLocationException(string? message) : base($"There is an error of the legacy of location. {message}")
+        {
+        }
+    }
+}
--- LootManagerApi/Repositories/LocationRepository.cs
+++ LootManagerApi/Repositories/LocationRepository.cs
@@ -1,5 +1,6 @@
 ﻿using LootManagerApi.Dto.LogisticsDto;
 using LootManagerApi.Entities.logistics;
+using LootManagerApi.Exceptions;
 using LootManagerApi.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
@@ -112,21 +113,21 @@ namespace LootManagerApi.Repositories
 
                 location.PositionId = locationUpdateDto.PositionId;
 
+                location.UpdatedAt = DateTime.UtcNow;
+
                 return new LocationDto(await UpdateLocationAsync(location));
             }
             catch (Exception ex)
             {
                 throw new Exception($"An error occurred while updating the location. {ex.Message}", ex);
             }
-
-            throw new NotImplementedException();
         }
 
         public async Task<Location> UpdateLocationAsync(Location location)
         {
-                await context.SaveChangesAsync();
+            await context.SaveChangesAsync();
 
-                return location;
+            return location;
         }
 
         #endregion
@@ -157,17 +158,14 @@ namespace LootManagerApi.Repositories
 
         public async Task<bool> IsLocationExistAsync(int locationId)
         {
-            if (await context.Locations.AnyAsync(l => l.Id == locationId))
-                return true;
+            return await context.Locations.AnyAsync(l => l.Id == locationId);
 
             throw new Exception($"This location does not exist in the database. Location Id  : {locationId}");
         }
 
         public async Task<bool> CheckOwnerOfLocationAsync(int userId, int locationId)
         {
-            await context.Locations.AnyAsync(x => x.UserId == userId && x.Id == locationId);
-
-            return true;
+            return await context.Locations.AnyAsync(x => x.UserId == userId && x.Id == locationId);
 
             throw new Exception($"This user cannot access this location. User Id : {userId}. Location Id : {locationId}");
         }
@@ -176,11 +174,11 @@ namespace LootManagerApi.Repositories
         {
             try
             {
-                return await CheckLocationHierarchyAsync(new LocationChain(locationCreateDto));
+                return await CheckLegacyOfLocationAsync(new LocationLegacy(locationCreateDto));
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                throw new LegacyOfLocationException(ex.Message);
             }
         }
 
@@ -188,16 +186,16 @@ namespace LootManagerApi.Repositories
         {
             try
             {
-                return await CheckLocationHierarchyAsync(new LocationChain(locationUpdateDto));
+                return await CheckLegacyOfLocationAsync(new LocationLegacy(locationUpdateDto));
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                throw new LegacyOfLocationException(ex.Message);
             }
 
         }
 
-        private async Task<bool> CheckLocationHierarchyAsync(LocationChain locationChain)
+        private async Task<bool> CheckLegacyOfLocationAsync(LocationLegacy locationLegacy)
         {
             var userTables = await context.Users.AsNoTracking().
                 Include(u => u.Houses).
@@ -205,51 +203,51 @@ namespace LootManagerApi.Repositories
                 Include(u => u.Furnitures).
                 Include(u => u.Shelves).
                 Include(u => u.Positions).
-                Where(u => u.Id == locationChain.UserId).FirstAsync();
+                Where(u => u.Id == locationLegacy.UserId).FirstAsync();
 
-            if (locationChain.PositionId != null)
+            if (locationLegacy.PositionId != null)
             {
-                if (userTables.Positions == null || !userTables.Positions.Any(p => p.Id == locationChain.PositionId))
-                    throw new Exception("This position is not accessible for this user.");
+                if (userTables.Positions == null || !userTables.Positions.Any(p => p.Id == locationLegacy.PositionId))
+                    throw new Exception($"This position is not accessible for this user. PositionId: {locationLegacy.PositionId}; UserId: {locationLegacy.UserId};");
 
-                if (!userTables.Positions.Any(p => p.Id == locationChain.PositionId && p.ShelfId == locationChain.ShelfId))
-                    throw new Exception("This position is not accessible for this shelf.");
+                if (!userTables.Positions.Any(p => p.Id == locationLegacy.PositionId && p.ShelfId == locationLegacy.ShelfId))
+                    throw new Exception($"This position is not accessible for this shelf. PositionId: {locationLegacy.PositionId}; ShelfId: {locationLegacy.ShelfId};");
             }
 
-            if (locationChain.ShelfId != null)
+            if (locationLegacy.ShelfId != null)
             {
-                if (userTables.Shelves == null || !userTables.Shelves.Any(s => s.Id == locationChain.ShelfId))
-                    throw new Exception("This shelf is not accessible for this user.");
+                if (userTables.Shelves == null || !userTables.Shelves.Any(s => s.Id == locationLegacy.ShelfId))
+                    throw new Exception($"This shelf is not accessible for this user. ShelfId: {locationLegacy.ShelfId}; UserId: {locationLegacy.UserId};");
 
-                if (!userTables.Shelves.Any(s => s.Id == locationChain.ShelfId && s.FurnitureId == locationChain.FurnitureId))
-                    throw new Exception("This shelf is not accessible for this furniture.");
+                if (!userTables.Shelves.Any(s => s.Id == locationLegacy.ShelfId && s.FurnitureId == locationLegacy.FurnitureId))
+                    throw new Exception($"This shelf is not accessible for this furniture. ShelfId: {locationLegacy.ShelfId}; FurnitureId: {locationLegacy.FurnitureId};");
             }
 
-            if (locationChain.FurnitureId != null)
+            if (locationLegacy.FurnitureId != null)
             {
-                if (userTables.Furnitures == null || !userTables.Furnitures.Any(f => f.Id == locationChain.FurnitureId))
-                    throw new Exception("This furniture is not accessible for this user.");
+                if (userTables.Furnitures == null || !userTables.Furnitures.Any(f => f.Id == locationLegacy.FurnitureId))
+                    throw new Exception($"This furniture is not accessible for this user. FurnitureId: {locationLegacy.FurnitureId}; UserId: {locationLegacy.UserId};");
 
-                if (!userTables.Furnitures.Any(f => f.Id == locationChain.FurnitureId && f.RoomId == locationChain.RoomId))
-                    throw new Exception("This furniture is not accessible for this room.");
+                if (!userTables.Furnitures.Any(f => f.Id == locationLegacy.FurnitureId && f.RoomId == locationLegacy.RoomId))
+                    throw new Exception($"This furniture is not accessible for this room. FurnitureId: {locationLegacy.FurnitureId}; RoomId: {locationLegacy.RoomId};");
             }
 
-            if (locationChain.RoomId != null)
+            if (locationLegacy.RoomId != null)
             {
-                if (userTables.Rooms == null || !userTables.Rooms.Any(r => r.Id == locationChain.RoomId))
-                    throw new Exception("This room is not accessible for this user.");
+                if (userTables.Rooms == null || !userTables.Rooms.Any(r => r.Id == locationLegacy.RoomId))
+                    throw new Exception($"This room is not accessible for this user. RoomId: {locationLegacy.RoomId}; UserId: {locationLegacy.UserId};");
 
-                if (!userTables.Rooms.Any(r => r.Id == locationChain.RoomId && r.HouseId == locationChain.HouseId))
-                    throw new Exception("This room is not accessible for this house.");
+                if (!userTables.Rooms.Any(r => r.Id == locationLegacy.RoomId && r.HouseId == locationLegacy.HouseId))
+                    throw new Exception($"This room is not accessible for this house. RoomId: {locationLegacy.RoomId}; HouseId: {locationLegacy.HouseId};");
             }
 
-            if (!userTables.Houses.Any(h => h.Id == locationChain.HouseId))
-                throw new Exception("This house is not accessible for this user.");
+            if (!userTables.Houses.Any(h => h.Id == locationLegacy.HouseId))
+                throw new Exception($"This house is not accessible for this user. HouseId: {locationLegacy.HouseId}; UserId: {locationLegacy.UserId};");
 
             return true;
         }
 
-        private class LocationChain
+        private class LocationLegacy
         {
             [Required] public int UserId { get; set; }
             [Required] public int HouseId { get; set; }
@@ -258,7 +256,7 @@ namespace LootManagerApi.Repositories
             public int? ShelfId { get; set; }
             public int? PositionId { get; set; }
 
-            public LocationChain(LocationUpdateDto locationUpdateDto)
+            public LocationLegacy(LocationUpdateDto locationUpdateDto)
             {
                 UserId = locationUpdateDto.UserId;
                 HouseId = locationUpdateDto.HouseId;
@@ -268,7 +266,7 @@ namespace LootManagerApi.Repositories
                 PositionId = locationUpdateDto.PositionId;
             }
 
-            public LocationChain(LocationCreateDto locationCreateDto)
+            public LocationLegacy(LocationCreateDto locationCreateDto)
             {
                 UserId = locationCreateDto.UserId;
                 HouseId = locationCreateDto.HouseId;
